cc1b8ff8e399235d91417d02330c5a406ef5aaea
mattbialko <51932848+mattbialko@users.noreply.github.com>
2026-02-12 15:46:51 -0500

    Fixed minor build issues

--- src/main/java/frc/robot/subsystems/swervedrive/TurretSubsystem/Turret_Shoot.java
+++ src/main/java/frc/robot/subsystems/swervedrive/TurretSubsystem/Turret_Shoot.java
@@ -33,17 +33,17 @@ public class Turret_Shoot {
     TalonFX Shoot_Motor = new TalonFX(74); 
     DigitalInput testinput2026 = new DigitalInput(2);
     XboxController driveController = new XboxController(0);
-    armMotor = new TalonFX(3);
+    TalonFX armMotor = new TalonFX(3);
     //wheel spin
     public void setRPM(){
 
         if (driveController.getRightTriggerAxis()> 0 && testinput2026.get()){
 
-           TalonFX.limitSensorBottom.get() && AngularVelocityUnit.RPM > 40 && RPM < 30){
-        System.out.println("reach MAX speed");
+           //TalonFX.limitSensorBottom.get() && AngularVelocityUnit.RPM > 40 && RPM < 30){
+        //System.out.println("reach MAX speed");
 
         //RPM = 0;
-        }
+        //}
         
     }
 
