34a1c995a3b0e10b8a4037a6b5f43a4ac6ed6816
tsukikage <wmaurer616@gmail.com>
2026-03-03 19:47:09 -0500

    testing if its a allience got inverted

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -222,7 +222,7 @@ public class SwerveSubsystem extends SubsystemBase
       SmartDashboard.putData("Field", m_field);
       
       if (result.valid) {
-        Pose2d usefulPose = result.getBotPose2d(Alliance.Blue);
+        Pose2d usefulPose = result.getBotPose2d(Alliance.Red); //testing if its inverted from blue to Red
         double distanceToPose = usefulPose.getTranslation().getDistance(swerveDrive.getPose().getTranslation());
         if (distanceToPose < 0.5 || (outofAreaReading > 10) || (outofAreaReading > 10 && !initialReading)) {
           if (!initialReading) {
