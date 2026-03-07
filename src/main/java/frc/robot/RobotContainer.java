b8024f30e69e758dd685576f06bc34c6b857ca6b
prog1 <prog1@Team74>
2026-03-07 15:05:43 -0500

    control changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -220,6 +220,10 @@ void LEDs() {
     operatorXbox.leftBumper().onTrue(hoodSubsystem.MoveHoodOut()).whileFalse(hoodSubsystem.StopHood());
     // reverse
     operatorXbox.rightBumper().onTrue(hoodSubsystem.MoveHoodIn()).whileFalse(hoodSubsystem.StopHood());
+    
+    //test elevator code DELETE AFTER OR CHANGE
+    //operatorXbox.y().onTrue(shootSubsystem.testElevatorUp()).onFalse(shootSubsystem.testElevatorStop());
+    //operatorXbox.x().onTrue(shootSubsystem.testElevatorDown()).onFalse(shootSubsystem.testElevatorStop());
   }
 
   void IntakeSubsystem() {
